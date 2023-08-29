8cec17f27cf4ff8606e2f863de2169770763d72b
BrunoCroso <croso.bruno@usp.br>
2023-08-29 20:51:34 -0300

    Adição de truncation no embedder do BERTweet

--- fakenews/embeddings.py
+++ fakenews/embeddings.py
@@ -12,7 +12,7 @@ class UserEmbedder:
     def __init__(self, bertweet_model, not_in_vocabulary_embedding: Union[List, np.ndarray]=None):
         self.__bertweet_model = bertweet_model
         self.__embedding_dimensions = 768
-        self.__tokenizer = AutoTokenizer.from_pretrained("vinai/bertweet-base", use_fast=False)
+        self.__tokenizer = AutoTokenizer.from_pretrained("vinai/bertweet-base", use_fast=True)
         if not_in_vocabulary_embedding is None:
             self.__not_in_vocabulary_embedding = np.zeros(self.__embedding_dimensions)
         else:
@@ -22,7 +22,7 @@ class UserEmbedder:
         embedding = np.array([])
         if self.__bertweet_model is not None:
 
-            tokens = torch.tensor([self.__tokenizer.encode(user.description)])
+            tokens = torch.tensor([self.__tokenizer.encode(user.description, truncation=True)])
             with torch.no_grad():
                 word_embeddings = self.__bertweet_model(tokens)[0][0]
 
